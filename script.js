166bbbde0a167b14a45342b65450dcdce1de0ee6
Wangstaboi <bubmom9@gmail.com>
2022-08-15 14:09:06 -0700

    Initial commit; added requisite files

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+var playerScore = 0;
+var computerScore = 0;
+
+
+function getComputerChoice() {
+    let num = Math.random();
+    if (num <= 0.3) {
+        return 'rock';
+    }
+    else if (num <= 0.6) {
+        return 'scissors';
+    }
+    else if (num >= 0.6){
+        return 'paper';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    
+    if (player == computerSelection) {
+        return "Tie Game!"
+    }
+    
+    if (
+        (player === 'rock' && computerSelection === 'scissors') ||
+        (player === 'scissors' && computerSelection === 'paper') ||
+        (player === 'paper' && computerSelection === 'rock')
+      ) {
+        playerScore++;
+        return `You Win! ${player} beats ${computerSelection}`
+      }
+
+    if (
+    (computerSelection === 'rock' && player === 'scissors') ||
+    (computerSelection === 'scissors' && player === 'paper') ||
+    (computerSelection === 'paper' && player === 'rock')
+    ) {
+        computerScore++;
+        return `You Win! ${computerSelection} beats ${player}`
+    }
+};
+
+function game() {
+    let playerSelect = prompt("Enter Rock, Paper, or Scissors!");
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelect, getComputerChoice());
+    }
+
+    if (playerScore > computerScore) {
+        return 'You Win!';
+    }
+    else if(computerScore > playerScore) {
+        return 'You Lose!';
+    }
+}
+
+
+
+
+console.log(game());
